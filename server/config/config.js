1b3f2d77bec3e20f3aff0b79791464bd1b67bbbe
Wilbert <winco117@gmail.com>
2018-09-11 11:22:02 -0500

    Variable de entorno Mongo

--- server/config/config.js
+++ server/config/config.js
@@ -13,6 +13,6 @@ let urlDB;
 if (process.env.NODE_ENV === 'DEV') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-    urlDB = 'mongodb://USERCAFE:QWERTY1@ds139951.mlab.com:39951/cafe-wp';
+    urlDB = process.env.MONGO_URI;
 }
 process.env.URLDB = urlDB;
