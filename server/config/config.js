eafad129950f08c6a342bfee25800b33ca3adb63
Wilbert <winco117@gmail.com>
2018-09-11 10:54:23 -0500

    RESTSERVER - USUARIO

--- server/config/config.js
+++ server/config/config.js
@@ -2,3 +2,17 @@
  * PUERTO
   ================*/
 process.env.PORT = process.env.PORT || 3000;
+/*================
+ * Entorno
+  ================*/
+process.env.NODE_ENV = process.env.NODE_ENV || 'DEV';
+/*================
+ * Base de datos
+  ================*/
+let urlDB;
+if (process.env.NODE_ENV === 'DEV') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else {
+    urlDB = 'mongodb://USERCAFE:QWERTY1@ds139951.mlab.com:39951/cafe-wp';
+}
+process.env.URLDB = urlDB;
